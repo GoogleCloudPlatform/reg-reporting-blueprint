86288cb924ab24ba668007b0f2cecff48a775070
gts-trading-analytics <trading_analytics@gtsx.com>
2022-09-22 12:08:27 -0400

    Add copyright header

--- use_cases/examples/flashing_detection/dbt/models/induced_market_movement.sql
+++ use_cases/examples/flashing_detection/dbt/models/induced_market_movement.sql
@@ -1,8 +1,22 @@
+-- Copyright 2022 The Reg Reporting Blueprint Authors
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     https://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 SELECT
     take.*,
     nbbo.max_bid,
     nbbo.min_ask
-  FROM 
+  FROM
     {{ ref('take_events') }} AS take
     JOIN {{ ref('nbbo_around_fill') }} AS nbbo ON
         take.trade_date = nbbo.trade_date
--- use_cases/examples/flashing_detection/dbt/models/latest_nbbo.sql
+++ use_cases/examples/flashing_detection/dbt/models/latest_nbbo.sql
@@ -1,14 +1,28 @@
-SELECT 
-    take_events.trade_date, 
+-- Copyright 2022 The Reg Reporting Blueprint Authors
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     https://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+SELECT
+    take_events.trade_date,
     take_events.symbol,
-    take_exec_id, 
+    take_exec_id,
     MAX(nbbo.timestamp) AS nbbo_time
-  FROM 
-    {{ ref('take_events') }}, 
+  FROM
+    {{ ref('take_events') }},
     {{ source('flashing_detection_source_market_data', 'nbbo') }} as nbbo
-  WHERE 
+  WHERE
     nbbo.timestamp < take_timestamp
-  GROUP BY 
+  GROUP BY
     take_events.trade_date,
     take_events.symbol,
     take_exec_id,
