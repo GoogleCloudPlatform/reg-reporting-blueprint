56d5a87fcebf6a1e4f8fb2e4cb0478e83d4c65c1
Mark Scannell <scannell@google.com>
2023-01-03 17:24:29 +0000

    Added docs generation

--- use_cases/examples/home_loan_delinquency/dbt/dbt_generate_docs.py
+++ use_cases/examples/home_loan_delinquency/dbt/dbt_generate_docs.py
@@ -6,6 +6,8 @@ import json
 import subprocess
 import argparse
 
+from google.cloud.storage import client, bucket, blob
+
 
 #
 # Generate static documentation
@@ -59,14 +61,32 @@ parser.add_argument('--dbt_project_dir', nargs='?',
                     default=os.path.dirname(sys.argv[0]))
 parser.add_argument('--build_ref', nargs='?',
                     default=os.environ.get('BUILD_REF', 'unset'))
-parser.add_argument('--commit_sha', nargs='?',
-                    default=os.environ.get('COMMIT_SHA', 'unset'))
+parser.add_argument('--save_gcs', nargs='?',
+                    default=os.environ.get('GCS_DOCS_BUCKET', None))
 
 args = parser.parse_args()
 
-generate_dbt_docs(args.dbt_project_dir)
+#
+# Save static documentation to GCS
+#
+def save_static_docs(gcs_bucket, gcs_object, dbt_project_dir):
+
+  from google.cloud.storage import client
+
+  source_file = os.path.join(dbt_project_dir, 'target', 'static-index.html')
+  print(f'Uploading {source_file} to object gs://{gcs_bucket}/{gcs_object} ')
+  target_blob = client.Client().bucket(gcs_bucket).blob(gcs_object)
+  target_blob.upload_from_filename(source_file)
+
+
+
+# generate_dbt_docs(args.dbt_project_dir)
+
 generate_static_docs(args.dbt_project_dir, subs={
     '%{BULID_REF}':  args.build_ref,
     '%{COMMIT_SHA}': args.commit_sha,
 })
+
+if args.save_gcs:
+  save_static_docs(args.save_gcs, 'build/' + args.build_ref + '/index.html', args.dbt_project_dir)
 
